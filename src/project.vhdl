ba6cd393555b651fa879c038959ef498893bd54b
josvo509 <josvo509@muxen2-111.ad.liu.se>
2025-11-08 17:13:20 +0100

    implemented 8 bit XOR
    used algorithm from https://github.com/tzarc/qmk_userspace/blob/bebe5e5b21e99bdb8ff41500ade1eac2d8417d8c/users-tzarc/tzarc_common.c#L57-L63
    
    Committer: noreply <noreply@noreply>

--- src/project.vhdl
+++ src/project.vhdl
@@ -16,10 +16,62 @@ entity tt_um_example is
 end tt_um_example;
 
 architecture Behavioral of tt_um_example is
+	signal counter : unsigned(7 downto 0) := "00000000";
+	signal state : std_logic_vector(7 downto 0) := "00000000";
+	signal previous_state : std_logic_vector(7 downto 0) := "00000000";
+
+	signal a : std_logic_vector(7 downto 0);
+	signal b : std_logic_vector(7 downto 0);
+	signal c : std_logic_vector(7 downto 0);
 begin
 
-    uo_out <= std_logic_vector(unsigned(ui_in) + unsigned(uio_in));
-    uio_out <= "00000000";
-    uio_oe <= "00000000";
+	process(rst_n, clk) begin
+		--if not rising_edge(rst_n) then 
+		if rst_n = '0' then
+			counter <= "00000000";
+			state <= "00000000";
+			previous_state <= "00000000";
+		elsif rising_edge(clk) then
+			state <= c;
+
+			if ui_in = previous_state then
+				counter <= counter + 1;
+			else 
+				previous_state <= ui_in;
+				state <= ui_in;
+				counter <= "00000000";
+			end if;
+		end if;
+	end process;
+
+	--a <= ui_in xor ("000" & ui_in(ui_in'high downto 3));
+	--b <= a xor (a(a'high-5 downto 0) & "00000");
+	--c <= b xor std_logic_vector(counter(counter'high-2 downto 0) & "00");
+
+	a <= state xor (state(state'high-3 downto 0) & "000");
+	b <= a xor ("00000"& a(a'high downto 5));
+	c <= b xor std_logic_vector("00" & counter(counter'high downto 2));
+
+	-- uo_out <= std_logic_vector(unsigned(ui_in) + unsigned(uio_in));
+	uo_out <= c;
+	-- state <= c;
+
+	uio_out <= "00000000";
+	uio_oe <= "00000000";
 
 end Behavioral;
+
+-- int main()
+-- {
+	-- static uint8_t s = 2, a = 0;
+-- 
+	-- for(unsigned long i=0;;i++)
+	-- {
+-- 
+		-- s ^= s << 3;
+		-- s ^= s >> 5;
+		-- s ^= a++ >> 2;
+-- 
+		-- printf("%i\n", s);
+	-- }
+--}
--- test/test.py
+++ test/test.py
@@ -23,18 +23,43 @@ async def test_project(dut):
     await ClockCycles(dut.clk, 10)
     dut.rst_n.value = 1
 
-    dut._log.info("Test project behavior")
+    dut._log.info("Test seed")
 
-    # Set the input values you want to test
-    dut.ui_in.value = 20
-    dut.uio_in.value = 30
-
-    # Wait for one clock cycle to see the output values
+    # set seed, Moore so wait one cyckle for value to be saved
+    dut.ui_in.value = 210
     await ClockCycles(dut.clk, 1)
 
-    # The following assersion is just an example of how to check the output values.
-    # Change it to match the actual expected output of your module:
-    assert dut.uo_out.value == 50
+    # verify that seed is correct with output
+    for i, expected in enumerate([64,66,80,214,100,71,125,144,18,132,163,188,93,179,41,97,110,26,200,136]):
+        await ClockCycles(dut.clk, 1)
+        assert dut.uo_out.value == expected
 
-    # Keep testing the module by changing the input values, waiting for
-    # one or more clock cycles, and asserting the expected output values.
+    dut._log.info("Test switching seed")
+
+    # switching the seed, for one Moore so the get one more of the last sequence
+    dut.ui_in.value = 2
+    await ClockCycles(dut.clk, 1)
+    assert int(dut.uo_out.value) == 203;
+
+    # check if output is correct with the new seed
+    expected_random = [18,134,179,42,120,188,95,163,188,92,187,98,114,230,211,74,30,237,133,172,207]
+    for i, expected in enumerate(expected_random):
+        await ClockCycles(dut.clk, 1)
+        assert dut.uo_out.value == expected
+
+    dut._log.info("Test reset")
+
+    # we reset for one cycle, Moore so wait one until
+    # the expected value is returned
+    dut.rst_n.value = 0;
+    await ClockCycles(dut.clk, 1)
+    dut.rst_n.value = 1;
+    await ClockCycles(dut.clk, 1)
+
+    for i, expected in enumerate(expected_random):
+        await ClockCycles(dut.clk, 1)
+        assert dut.uo_out.value == expected
+
+
+
+
